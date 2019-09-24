51a92d74c5b26e38b2f0188727ad7c4303c1767a
Gabriely Poian <42159614+gabrielypoian@users.noreply.github.com>
2019-09-24 19:34:10 -0300

    Add files via upload

---
+++ lista-de-filmes.php
@@ -0,0 +1,14 @@
+<?php
+
+$infoAvengers = array('Vingadores', '180m', '2019');
+$infoCaptain = array('Capitã marvel', '120m', '2019');
+$infoVespa = array('Homem Formiga', '120m', '2019');
+
+list($Nome, $duracao, $ano) = $infoAvengers;
+echo "$Nome, $duracao e $ano. <br>";
+
+list($Nome, $duracao, $ano) = $infoCaptain;
+echo "$Nome, $duracao e $ano. <br>";
+
+list($Nome, $duracao, $ano) = $infoVespa;
+echo "$Nome, $duracao e $ano. <br>";
